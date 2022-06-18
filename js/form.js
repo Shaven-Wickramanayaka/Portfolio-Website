6db3fd7847ef8f444c50b9423ff0b292a577a9a1
IceFly147 <100683747+IceFly147@users.noreply.github.com>
2022-06-18 11:58:47 +0530

    Added form functionality :}

--- js/form.js
+++ js/form.js
@@ -1,1 +1,28 @@
+// % emailjs stuff :)
+// ? https://dashboard.emailjs.com/admin/account
+
+const public = "ZwsR1qFB3TMYkhSVV";
+const gmail = "Personal_Website";
+const template = "template_w7o3tje";
+
+(function () {
+  emailjs.init(public);
+})();
+
+window.onload = function () {
+  document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    this.contact_number.value = (Math.random() * 100000) | 0;
+    // these IDs from the previous steps
+    emailjs.sendForm(gmail, template, this).then(
+      function () {
+        alert("Your Email was successfully sent :)");
+      },
+      function (error) {
+        alert("Your Email was was not successfully sent :(", error);
+      }
+    );
+  });
+};
 
